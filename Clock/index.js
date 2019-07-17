3fc1478451e5a80bc669445f52f7d0fa8605c1d4
Richard Shelton <richard.shelton567@gmail.com>
2019-07-17 14:22:46 -0400

    Javascript Clock

---
+++ Clock/index.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const setDate = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+// As name implies, activates the function passed in to the number of ms 
+setInterval(setDate, 1000);
+
